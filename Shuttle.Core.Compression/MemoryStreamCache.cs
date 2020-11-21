6cc307353e2728c2fe256107a555df4617519a86
Gábor Zavarkó <gabor.zavarko@horizonte.as>
2020-11-21 15:01:12 +0100

    using Microsoft.IO.RecyclableMemoryStream

---
+++ Shuttle.Core.Compression/MemoryStreamCache.cs
@@ -0,0 +1,9 @@
+using Microsoft.IO;
+
+namespace Shuttle.Core.Compression
+{
+    internal static class MemoryStreamCache
+    {
+        public static readonly RecyclableMemoryStreamManager Manager = new RecyclableMemoryStreamManager();
+    }
+}
