3d1ced104059915e6d0d757c39691f994f482e83
Eben <me@ebenroux.co.za>
2020-11-23 07:24:19 +0200

    - extensions

---
+++ Shuttle.Core.Compression/CompressionAlgorithmExtensions.cs
@@ -0,0 +1,33 @@
+﻿using System.IO;
+using Shuttle.Core.Contract;
+
+namespace Shuttle.Core.Compression
+{
+    public static class CompressionAlgorithmExtensions
+    {
+        public static Stream Compress(this ICompressionAlgorithm algorithm, Stream stream)
+        {
+            Guard.AgainstNull(algorithm, nameof(algorithm));
+            Guard.AgainstNull(stream, nameof(stream));
+            
+            using (var ms = new MemoryStream())
+            {
+                stream.CopyTo(ms);
+
+                return new MemoryStream(algorithm.Compress(ms.ToArray()));
+            }
+        }
+        public static Stream Decompress(this ICompressionAlgorithm algorithm, Stream stream)
+        {
+            Guard.AgainstNull(algorithm, nameof(algorithm));
+            Guard.AgainstNull(stream, nameof(stream));
+            
+            using (var ms = new MemoryStream())
+            {
+                stream.CopyTo(ms);
+
+                return new MemoryStream(algorithm.Decompress(ms.ToArray()));
+            }
+        }
+    }
+}
